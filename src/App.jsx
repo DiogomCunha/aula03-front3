22f812b371df77b28e2f8ecc7c2b41547d88e753
DiogomCunha <ddmoraes.ddm@gmail.com>
2023-02-15 23:15:21 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './style.css'
+
+function App() {
+
+
+  return (
+    <div>
+   <div className="container">
+    <div className="cabecalho">
+      <h1>Mesa Trabalho 3</h1>
+    </div>
+   </div>
+    <div className="corpo">
+    <h2>Lorem ipsum</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.</p>
+  </div>
+  <div className="foother">
+    <h3>Todos os direitos reservados 2022</h3>
+  </div>
+  </div>
+  )
+}
+
+export default App
